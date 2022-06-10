b868d9bcf72e652b1877432c324a758088124534
buryianton <buryyanton@gmail.com>
2022-06-10 20:43:08 +0300

    add tests

--- main.py
+++ main.py
@@ -94,33 +94,4 @@ while True:
         Pushkin()
 
     if choice == '10':
-        while True:
-            print('1. пополнение счета')
-            print('2. покупка')
-            print('3. история покупок')
-            print('4. выход')
-
-            choice = input('Выберите пункт меню: ')
-
-            if choice == '1':
-                money = int(input("Введите сумму: "))
-                account += money
-                print('На счету ', account, 'р.')
-            elif choice == '2':
-                money = int(input('Введите сумму покупки: '))
-                amount.append(money)
-                if money < account:
-                    print('Не хватает средств на счете')
-                elif money > account:
-                    account -= money
-                purchase = input('Введите назначение покупки: ')
-                history.append(purchase)
-
-            elif choice == '3':
-                for i in range(len(history)):
-                    print(history[i], ' - ', amount[i])
-
-            elif choice == '4':
-                break
-            else:
-                print('Неверный пункт меню')
+        import bank
